104854160567c0406b22044fd322b13ff0bedd8a
aristocrat44 <arcrt1993@gmail.com>
2025-09-16 20:10:37 +0545

    env minor change

--- frontend/app/api/verify/route.ts
+++ frontend/app/api/verify/route.ts
@@ -1,4 +1,5 @@
-const API_BASE_URL = process.env.BACKEND_URL || "http://localhost:3000/api";
+const API_BASE_URL =
+  process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:3000/api";
 
 export async function POST(request: Request) {
   try {
